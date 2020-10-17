b32619d630634f399dceaf11cf408cc665cd7811
sufiblues <ali.hamdani.developer@gmail.com>
2020-10-17 17:56:50 -0500

    Added game board, added interaction with board (light up which board user clicked)

---
+++ detective_bayes/detective_bayes/game.h
@@ -0,0 +1,96 @@
+//
+//  game.h
+//  astroid
+//
+//  Created by Ali Hamdani on 10/11/20.
+//
+
+#ifndef game_h
+#define game_h
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+
+typedef struct Controller{
+    int edit;
+    int mouse_x;
+    int mouse_y;
+
+}Controller;
+
+typedef struct Game{
+    int board[16][9];
+    int screen_width;
+    int screen_height;
+    Controller control;
+}Game;
+
+Game game;
+
+
+//need to handle input in an intuitive way
+void inputs( bool* a, struct Controller* xbone){
+    SDL_Event event;
+    
+    while (SDL_PollEvent(&event)){
+        switch (event.type) {
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym)
+                {
+                    case SDLK_e:
+                        xbone->edit = 1;
+                        break;
+                }
+                break;
+            case SDL_KEYUP:
+                switch(event.key.keysym.sym)
+                {
+
+                    case SDLK_e:
+                        xbone->edit = 0;
+                        break;
+
+                }
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                SDL_GetMouseState(&xbone->mouse_x, &xbone->mouse_y);
+
+                break;
+            case SDL_MOUSEBUTTONUP:
+                xbone->mouse_x = -1;
+                xbone->mouse_y = -1;
+                break;
+            case SDL_QUIT:
+                *a = true;
+        }
+    }
+}
+void boardInit(){
+    for (int i = 0; i < 16; i++){
+        for (int j = 0; j < 9; j++){
+            game.board[i][j] = -1;
+        }
+    }
+}
+void gameInit(int width, int height){
+    boardInit();
+    game.screen_width = width;
+    game.screen_height = height;
+}
+
+//TODO: Change 46 to a generalized tilesize variable
+SDL_Point whichTile(int x, int y){
+    SDL_Point point = {x/46,y/46};
+    return point;
+}
+
+void updateBoard(){
+    if ((game.control.mouse_x > 0) && (game.control.mouse_y > 0)){
+        SDL_Point tile = whichTile(game.control.mouse_x, game.control.mouse_y);
+        printf("(col:%d,row:%d)\n" , tile.x , tile.y);
+        game.board[tile.y][tile.x] = 1;
+    }
+}
+
+#endif /* game_h */
--- detective_bayes/detective_bayes/main.c
+++ detective_bayes/detective_bayes/main.c
@@ -1,17 +1,18 @@
-//
-//  main.c
-//  detective_bayes
-//
-//  Created by Ali Hamdani on 10/17/20.
-//
-
-#include <stdio.h>
-#include <stdbool.h>
 #include <SDL2/SDL.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<math.h>
+#include "game.h"
 
 //Screen dimensions
 const int WINDOW_WIDTH = 414;
 const int WINDOW_HEIGHT = 736;
+const int NO_COLS = 9;
+const int NO_ROWS = 16;
+const double FRAME_RATE = 15.0;
+const double FRAME_DELAY = 1000/FRAME_RATE;
+
+
 
 //initialize sdl window
 bool init(void);
@@ -21,37 +22,115 @@ bool window_close(void);
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 
+//initializes window,rendrer,and surface
 bool init()
 {
     //Initialization flag
     bool success = true;
     //Initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
-        SDL_Log( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
         success = false;
     }
     else{
         //Create window
         gWindow = SDL_CreateWindow( "astroids", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN );
         if( gWindow == NULL ){
-            SDL_Log( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
             success = false;
         }
         else{
             //create renderer
             gRenderer = SDL_CreateRenderer(gWindow,-1,0);
             if(gRenderer == NULL){
-                SDL_Log("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
             }
         }
     }
     return success;
 }
 
+//destroys renderer and window
+void endDestroy()
+{
+    //Destroy window
+    SDL_DestroyRenderer(gRenderer);
+    gRenderer = NULL;
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+}
 
-int main(int argc, const char * argv[]) {
-  
+
+void drawRect(SDL_Rect rect,int col1,int col2, int col3){
+    SDL_SetRenderDrawColor(gRenderer, col1, col2, col3, SDL_ALPHA_TRANSPARENT);
+    SDL_RenderFillRect(gRenderer, &rect);
+}
+
+//TODO: Replace 46 with generic tilesize
+void drawBoard(){
+    SDL_SetRenderDrawColor(gRenderer, 255, 255, 245, SDL_ALPHA_OPAQUE);
+    for (int i = 0; i < 16; i++){
+        for (int j = 0; j < 9; j++){
+            if (game.board[i][j] > 0){
+                SDL_Rect temp = {j*46,i*46,46,46};
+                SDL_RenderFillRect(gRenderer, &temp);
+            }
+        }
+    }
+}
+
+//TODO: Change 46 to generic tilesize
+void drawGrid(int rows, int cols){
+    SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    for(int i =0; i< rows+1; i++){
+        SDL_RenderDrawLine(gRenderer, 0, (i*46), WINDOW_WIDTH, (i*46));
+    }
+    for(int i =0; i < cols+1; i++){
+        SDL_RenderDrawLine(gRenderer, (i*46), 0, (i*46), WINDOW_HEIGHT);
+    }
+}
+
+int main()
+{
+    int frame_time;
+    int frame_start;
+    
     init();
     
+    bool done = false;
+    
+    while (!done) {
+        
+        frame_start = SDL_GetTicks();
+        
+        inputs(&done, &game.control);
+        updateBoard();
+        
+        //draw background
+        if(game.control.mouse_x>0 && game.control.mouse_y > 0){
+            SDL_Point tile = whichTile(game.control.mouse_x, game.control.mouse_y);
+            printf("Clicked (%d,%d)\n" , tile.x, tile.y);
+        }
+        
+        SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        
+        drawGrid(NO_ROWS, NO_COLS);
+        drawBoard();
+    
+        SDL_RenderPresent(gRenderer);
+        
+        
+        frame_time = SDL_GetTicks() - frame_start;
+        if(FRAME_DELAY>frame_time){
+            SDL_Delay(FRAME_DELAY-frame_time);
+        }
+    }
+    
+    
+    endDestroy();
+    
+    SDL_Quit();
+     
+   
     return 0;
 }
